9f0ea42d27a7117b968c0510f52cb6737277c853
Tait Weinert <tait.weinert@outlook.com>
2022-06-07 17:52:47 +0930

    App changed to functional component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./styles/App.css";
 import Information from "./components/Information.js";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div id="app-div">
-        <Information heading="General Information"/>
-        <Information heading="Educational Experience"/>
-        <Information heading="Practical Experience"/>
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div id="app-div">
+      <Information heading="General Information"/>
+      <Information heading="Educational Experience"/>
+      <Information heading="Practical Experience"/>
+    </div>
+  );
 }
 
 export default App;
