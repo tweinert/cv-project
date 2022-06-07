474e83c826c10ec495bbd947bcac6e57da578cbe
Tait Weinert <tait.weinert@outlook.com>
2022-06-07 17:50:20 +0930

    Information component changed to functional component

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,72 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/Information.css";
 
-class Information extends React.Component {
+function Information(props) {
   /*
   Include paragraph, textarea, edit button, submit button.
   Use Display: none to switch between p + edit, and textarea + submit.
   */
-  constructor() {
-    super();
+  const [text, setText] =
+    useState(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+  ornare convallis sapien eget varius. Fusce eros nisi, posuere eu
+  eros id, faucibus finibus odio. Maecenas sagittis at est id
+  faucibus. Vestibulum vitae urna blandit, luctus lacus eu, mattis
+  nunc. Phasellus volutpat mauris in lectus viverra finibus. Nunc
+  sagittis arcu ac velit condimentum pretium. Suspendisse potenti. Sed
+  vitae nisi laoreet, ornare lectus ac, gravida turpis.`);
 
-    this.state = {
-      text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-      ornare convallis sapien eget varius. Fusce eros nisi, posuere eu
-      eros id, faucibus finibus odio. Maecenas sagittis at est id
-      faucibus. Vestibulum vitae urna blandit, luctus lacus eu, mattis
-      nunc. Phasellus volutpat mauris in lectus viverra finibus. Nunc
-      sagittis arcu ac velit condimentum pretium. Suspendisse potenti. Sed
-      vitae nisi laoreet, ornare lectus ac, gravida turpis.`
-    }
+  return (
+    <div className="information-div">
+      <form>
+        <h3>{props.heading}</h3>
+        <p id="paragraph">{text}</p>
+        <textarea id="textArea"></textarea>
+        <button type="button" id="editButton" onClick={handleEditButton}>
+          Edit
+        </button>
+        <button type="button" id="submitButton" onClick={handleSubmitButton}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 
-    this.handleEditButton = this.handleEditButton.bind(this);
-    this.handleSubmitButton = this.handleSubmitButton.bind(this);
-  }
-
-  render() {
-    return (
-      <div className="information-div">
-        <form>
-          <h3>{this.props.heading}</h3>
-          <p id="paragraph">{this.state.text}</p>
-          <textarea id="textArea"></textarea>
-          <button type="button" id="editButton" onClick={this.handleEditButton}>Edit</button>
-          <button type="button" id="submitButton" onClick={this.handleSubmitButton}>Submit</button>
-        </form>
-      </div>
-    );
-  }
-
-  handleEditButton() {
+  function handleEditButton() {
     const para = document.getElementById("paragraph");
     const textArea = document.getElementById("textArea");
     const editButton = document.getElementById("editButton");
     const submitButton = document.getElementById("submitButton");
-    
+
     textArea.style.display = "block";
     submitButton.style.display = "block";
 
     textArea.value = para.innerHTML;
-    
+
     para.style.display = "none";
     editButton.style.display = "none";
   }
-  
 
-  handleSubmitButton() {
+  function handleSubmitButton() {
     const para = document.getElementById("paragraph");
     const textArea = document.getElementById("textArea");
     const editButton = document.getElementById("editButton");
     const submitButton = document.getElementById("submitButton");
 
-    this.setState({
-      text: textArea.value
-    });
+    setText(textArea.value);
 
     para.style.display = "block";
     editButton.style.display = "block";
 
-    para.innerHTML = this.state.text;
+    para.innerHTML = text;
 
     textArea.style.display = "none";
     submitButton.style.display = "none";
